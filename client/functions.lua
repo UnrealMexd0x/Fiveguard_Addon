4c225782d866930224f5594d87ef7559d7a87a79
UnrealMexd0x <mariusholzer@mail.de>
2024-04-26 19:59:45 +0200

    v1.4.1

---
+++ client/functions.lua
@@ -0,0 +1,39 @@
+-- V1.4.1 Author UnrealMexd0x
+
+FiveguardAddon = FiveguardAddon or {}
+
+FiveguardAddon.Client = {
+    playerLoaded = false,
+    resourceWhitelist = {}
+}
+
+FiveguardAddon.Client.PLLoaded = function()
+    FiveguardAddon.Client.playerLoaded = true
+end
+
+FiveguardAddon.Client.checkWhitelist = function(resource)
+    return not FiveguardAddon.Client.playerLoaded or FiveguardAddon.Client.resourceWhitelist[resource]
+end
+
+FiveguardAddon.Client.addWhitelist = function(resource)
+    FiveguardAddon.Client.resourceWhitelist[resource] = true
+end
+
+FiveguardAddon.Client.removeWhitelist = function(resource)
+    FiveguardAddon.Client.resourceWhitelist[resource] = nil
+end
+
+FiveguardAddon.Client.ClientStop = function(resource)
+    if not FiveguardAddon.Client.checkWhitelist(resource) then
+        TriggerServerEvent("Fiveguard_Addon:BanPlayerStop", resource)
+    end
+
+    FiveguardAddon.Client.removeWhitelist(resource)
+end
+
+FiveguardAddon.Client.Stop = function(resource)
+    if GetInvokingResource() ~= nil then return end
+    FiveguardAddon.Client.addWhitelist(resource)
+end
+
+return FiveguardAddon.Client
--- client/main.lua
+++ client/main.lua
@@ -1,40 +1,8 @@
--- V1.4.0 Author UnrealMexd0x
+-- V1.4.1 Author UnrealMexd0x
 
 FiveguardAddon = FiveguardAddon or {}
 
-FiveguardAddon.Client = {
-    playerLoaded = false,
-    resourceWhitelist = {}
-}
-
-FiveguardAddon.CreateThread(function()
-    FiveguardAddon.Client.playerLoaded = true
-end)
-
-FiveguardAddon.Client.checkWhitelist = function(resource)
-    return not FiveguardAddon.Client.playerLoaded or FiveguardAddon.Client.resourceWhitelist[resource]
-end
-
-FiveguardAddon.Client.addWhitelist = function(resource)
-    FiveguardAddon.Client.resourceWhitelist[resource] = true
-end
-
-FiveguardAddon.Client.removeWhitelist = function(resource)
-    FiveguardAddon.Client.resourceWhitelist[resource] = nil
-end
-
-FiveguardAddon.Client.ClientStop = function(resource)
-    if not FiveguardAddon.Client.checkWhitelist(resource) then
-        TriggerServerEvent("Fiveguard_Addon:BanPlayerStop", resource)
-    end
-
-    FiveguardAddon.Client.removeWhitelist(resource)
-end
-
-FiveguardAddon.Client.Stop = function(resource)
-    if GetInvokingResource() ~= nil then return end
-    FiveguardAddon.Client.addWhitelist(resource)
-end
+FiveguardAddon.CreateThread(FiveguardAddon.Client.PLLoaded)
 
 FiveguardAddon.AddEvent("onClientResourceStop", FiveguardAddon.Client.ClientStop)
 FiveguardAddon.AddEvent("onResourceStop", FiveguardAddon.Client.Stop)
